847123d0d3521bd2d5320e7982e7d37fd60be373
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-12-20 16:32:11 -0300

    created router for test route

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,13 @@ import "reflect-metadata";
 
 import connectDatabase from "./database";
 
-import * as testController from "./controllers/testController";
+import testRouter from "./routers/testRouter";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/test", testController.createTest);
-app.get('/test/params', testController.findAvailableTestParams);
-app.get('/test/professors', testController.findProfessorsWithTests);
-app.get('/test/subjects', testController.findSubjectsWithTests);
+app.use('/test', testRouter);
 
 export async function init () {
   await connectDatabase();
